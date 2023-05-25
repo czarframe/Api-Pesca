2ef672974cf2d2529db694246cbdd33b49acb144
Cesar Augusto <cesaragstincs@gmail.com>
2023-05-25 12:22:25 -0300

    Commit inicial

---
+++ src/main/java/com/projeto/pesca/PescaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.pesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PescaApplication.class, args);
+	}
+
+}
